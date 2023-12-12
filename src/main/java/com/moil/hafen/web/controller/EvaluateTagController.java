1feb0762477833422b265797b132361f3295ea70
ChenZiJie <ChenZiJieJava@163.com>
2023-12-12 22:37:14 +0800

    fix：用户标签接口

--- src/main/java/com/moil/hafen/web/controller/EvaluateTagController.java
+++ src/main/java/com/moil/hafen/web/controller/EvaluateTagController.java
@@ -1,5 +1,7 @@
 package com.moil.hafen.web.controller;
 
+import cn.hutool.core.util.ObjectUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.moil.hafen.common.controller.BaseController;
 import com.moil.hafen.common.domain.QueryRequest;
@@ -16,7 +18,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author 8129
@@ -24,7 +25,7 @@ import java.util.Map;
 @Slf4j
 @RestController
 @RequestMapping({"evaluateTag"})
-@Api(tags = "课堂点评评价体系管理 - 管理后台/小程序")
+@Api(tags = "管理后台/内部管理/基础规则管理/课堂点评评价体系管理")
 public class EvaluateTagController extends BaseController {
 
     private String message;
@@ -34,15 +35,22 @@ public class EvaluateTagController extends BaseController {
 
     @GetMapping
     @ApiOperation("获取课堂点评评价体系列表（分页）")
-    public Map<String, Object> page(QueryRequest request, EvaluateTag evaluateTag) {
+    public Result<IPage<EvaluateTag>> page(QueryRequest request, EvaluateTag evaluateTag) {
         IPage<EvaluateTag> page = this.evaluateTagService.getPage(request, evaluateTag);
-        return getDataTable(page);
+        return Result.OK(page);
     }
 
-    @PostMapping
+    @PostMapping("add")
     @ApiOperation("添加课堂点评评价体系信息")
-    public Result add(EvaluateTag evaluateTag) throws FebsException {
+    public Result<Object> add(@RequestBody EvaluateTag evaluateTag) throws FebsException {
         try {
+            LambdaQueryWrapper<EvaluateTag> queryWrapper = new LambdaQueryWrapper<>();
+            queryWrapper.eq(EvaluateTag::getType, evaluateTag.getType());
+            queryWrapper.eq(EvaluateTag::getName, evaluateTag.getName());
+            EvaluateTag tag = evaluateTagService.getOne(queryWrapper);
+            if (ObjectUtil.isNotEmpty(tag)) {
+                return Result.error("该评分名称已存在,请勿重复");
+            }
             evaluateTag.setCreateTime(new Date());
             evaluateTag.setModifyTime(new Date());
             evaluateTag.setState(Status.上架.state);
@@ -54,9 +62,9 @@ public class EvaluateTagController extends BaseController {
         }
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("delete")
     @ApiOperation("删除课堂点评评价体系信息")
-    public Result delete(@PathVariable Integer id) throws FebsException {
+    public Result<Object> delete(@RequestParam("id") Integer id) throws FebsException {
         try {
             return Result.OK(this.evaluateTagService.removeById(id));
         } catch (Exception e) {
@@ -66,10 +74,17 @@ public class EvaluateTagController extends BaseController {
         }
     }
 
-    @PutMapping
+    @PutMapping("update")
     @ApiOperation("修改课堂点评评价体系信息")
-    public Result update(EvaluateTag evaluateTag) throws FebsException {
+    public Result<Object> update(@RequestBody EvaluateTag evaluateTag) throws FebsException {
         try {
+            LambdaQueryWrapper<EvaluateTag> queryWrapper = new LambdaQueryWrapper<>();
+            queryWrapper.eq(EvaluateTag::getType, evaluateTag.getType());
+            queryWrapper.eq(EvaluateTag::getName, evaluateTag.getName());
+            EvaluateTag tag = evaluateTagService.getOne(queryWrapper);
+            if (ObjectUtil.isNotEmpty(tag)) {
+                return Result.error("该评分名称已存在,请勿重复");
+            }
             evaluateTag.setModifyTime(new Date());
             return Result.OK(this.evaluateTagService.updateById(evaluateTag));
         } catch (Exception e) {
@@ -78,16 +93,17 @@ public class EvaluateTagController extends BaseController {
             return Result.error(message);
         }
     }
-    @GetMapping("/{id}")
+
+    @GetMapping("detail")
     @ApiOperation("通过ID获取课堂点评评价体系详情")
-    public Result<EvaluateTag> detail(@PathVariable Integer id) {
+    public Result<EvaluateTag> detail(@RequestParam("id") Integer id) {
         return Result.OK(this.evaluateTagService.getById(id));
     }
+
     @GetMapping("/list")
     @ApiOperation("获取课堂点评评价体系列表")
-    public Result list() {
+    public Result<List<EvaluateTag>> list() {
         List<EvaluateTag> list = this.evaluateTagService.list();
         return Result.OK(list);
     }
-
 }
--- src/main/java/com/moil/hafen/web/domain/EvaluateTag.java
+++ src/main/java/com/moil/hafen/web/domain/EvaluateTag.java
@@ -2,13 +2,21 @@ package com.moil.hafen.web.domain;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
 import java.util.Date;
 
+/**
+ * @Author 陈子杰
+ * @Description 课堂点评评价体系
+ * @Version 1.0.0
+ * @Date 2023/12/11 11:59
+ */
 @Data
 @TableName("t_evaluate_tag")
 @ApiModel("课堂点评评价体系")
@@ -17,10 +25,15 @@ public class EvaluateTag implements Serializable {
     private static final long serialVersionUID = 3526292856970448961L;
     @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
+    @ApiModelProperty("评分名称")
     private String name;
+    @ApiModelProperty("评分类型：0-教师评分；1-家长评分")
     private Integer type;
+    @ApiModelProperty("状态：0-上架;1-下架")
     private Integer state;
-
+    @ApiModelProperty("删除标识：0-正常;1-删除")
+    @TableLogic
+    private Integer delFlag;
     private Date createTime;
     private Date modifyTime;
     private transient String createTimeFrom;
